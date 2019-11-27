32390fd01b9f046e53aea2c86e71b5be0a10b46f
nkarmac <nick.karamanian@gmail.com>
2019-11-27 13:51:16 -0800

    removed language filters

--- scripts/viewcounts/post-views.scala
+++ scripts/viewcounts/post-views.scala
@@ -10,12 +10,6 @@ import scala.xml._
  
 
 val xml = new XMLEventReader(Source.stdin)
-val languages = Set("java","c","c++","python",".net", "c#","javascript", "sql","php", "assembly", "objective-c", "delphi", "perl","matlab", "ruby","vba",
-                "groovy","swift","go","r","sas" ,"abap","fortran", "dart","scala", 
-                "prolog", "lisp", "lua", "rust", "ada","f#", "apex", "kotlin", 
-                "scheme", "labview", "typescript", "julia", "awk", "haskell","clojure", 
-                "erlang", "bash", "elixir", "html", "vhdl", "verilog", "vb.net", "d", "plsql", 
-                "netlogo", "cobol", "actionscript-3", "vbscript", "powershell", "coffeescript",  "common-lisp", "erlang")
 
 def clean(at:Seq[Node]):String = {
   if (at == null) {
@@ -51,9 +45,7 @@ xml.filter{ event =>
   }
 }.foreach{ case(id, views, tags) =>
     tags.foreach{ t =>
-      if (languages.contains(t))  {
-        println(s"${id}|${views}|${t}")
-      }
+      println(s"${id}|${views}|${t}")
     }
 }
  
